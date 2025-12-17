b1bc39ca78e81a70cbf0266889b1ca23aba619df
Asadbek <atamirzayevasadbek90@gmail.com>
2025-12-17 21:23:58 +0500

    change

---
+++ app.js
@@ -0,0 +1,19 @@
+// 1. Sum of positive
+// function positiveSum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       sum += arr[i];
+//     } 
+//   }
+//   return sum;
+// }
+
+// 2.Sum Arrays
+// function sum(numbers) {
+//   return numbers.reduce((acc, curr) => acc + curr, 0);
+// }
+
+
+
+
