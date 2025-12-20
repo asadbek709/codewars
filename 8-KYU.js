a1c6211f177585b5d6d66bd23bfddd721d47d2d7
Asadbek <atamirzayevasadbek90@gmail.com>
2025-12-20 16:46:36 +0500

    change

--- 8-KYU.js
+++ 8-KYU.js
@@ -1,23 +1,47 @@
+// ===============================================================
 // 1. Sum of positive
 // function positiveSum(arr) {
 //   let sum = 0;
 //   for (let i = 0; i < arr.length; i++) {
 //     if (arr[i] > 0) {
 //       sum += arr[i];
-//     } 
+//     }
 //   }
 //   return sum;
 // }
 
+// ===============================================================
+
 // 2.Sum Arrays
 // function sum(numbers) {
-let num = 0
-    numbers.forEach((value) => {
-        if(value > 0) num += value
-    });
-    return num
+// let num = 0
+//     numbers.forEach((value) => {
+//         if(value > 0) num += value
+//     });
+//     return num
+// // }
+
+// ===============================================================
+
+// 3.Elementlarni olib tashlash
+// function removeEveryOther(arr){
+//   return arr.filter((value, index) => index % 2 === 0);
 // }
 
+// ===============================================================
 
+// 4.Vergul ajratgichlari bilan massiv elementlarini chop etish
+//  printArray = _ => _.join(",")
 
+// ===============================================================
 
+// 5.Sum aralash massiv
+// function sumMix(x) {
+//     let sum = 0;
+//     for (let i = 0; i < x.length; i++) {
+//         sum += Number(x[i]);
+//     }
+//     return sum;
+// }
+
+// ===============================================================
