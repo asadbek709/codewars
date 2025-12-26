42ff3a59ae46d4c5006942ebf479b1d0ca8a26e0
Asadbek <atamirzayevasadbek90@gmail.com>
2025-12-26 16:49:26 +0500

    change

--- 8-KYU.js
+++ 8-KYU.js
@@ -101,3 +101,89 @@
 //     })
 //   return sum
 // }
+
+
+
+// 1 masala ===============================
+
+// let people = [
+//   { id: 1, firstName: "Eshmat", lastName: "Toshmatov", age: 20, status: "Talaba" },
+//   { id: 2, firstName: "Gulmat", lastName: "Surmatov", age: 17, status: "O'quvchi" },
+//   { id: 3, firstName: "Dilshod", lastName: "Karimov", age: 25, status: "Ishchi" },
+//   { id: 4, firstName: "Malika", lastName: "Xoliqova", age: 30, status: "Uqituvchi" },
+//   { id: 5, firstName: "Zafar", lastName: "Bozorov", age: 19, status: "Talaba" },
+//   { id: 6, firstName: "Umida", lastName: "Raxmatova", age: 22, status: "Tadbirkor" },
+//   { id: 7, firstName: "Rustam", lastName: "Ziyodov", age: 35, status: "Dasturchi" },
+//   { id: 8, firstName: "Nodira", lastName: "Saidova", age: 28, status: "Muhandis" },
+//   { id: 9, firstName: "Javlon", lastName: "Qurbonov", age: 24, status: "Frilanser" },
+//   { id: 10, firstName: "Shahnoza", lastName: "Ergasheva", age: 18, status: "O'quvchi" }
+// ];
+// let almashtirish = people.map(obj => {
+//   if (obj.id === 5) {
+//     return {
+//       id: 5,
+//       firstName: "Ali",
+//       lastName: "Valiyev",
+//       age: 25,
+//       status: "Dasturchi"
+//     };
+//   }
+//   return obj;
+// });
+
+// console.log(almashtirish);
+
+// ==============================================================================================
+
+// 2 masalaaaaaaaa
+
+// let obyect = people.filter((obj)=>{
+//     return obj.lastName.endsWith("va")
+// })
+// console.log(obyect );
+
+// ===============================================================
+// 3 masalaaaaaaaaaa
+
+// let keraklisiniOLish = people.filter((obj)=>{
+//     return obj.id !== 4 &&  obj.id !== 9
+// })
+// console.log(keraklisiniOLish);
+
+// ============================================================
+
+// 4 MASALAAAAAA
+
+// let isminiTanlash = people.find(obj => obj.id === 7);
+// isminiTanlash.firstName = "Gulbashakar";
+// console.log(isminiTanlash);
+
+// ========================================================================
+
+// 5 MASALAAAAA
+
+
+
+
+// let sortlash = people.sort((a, b)=>{
+//     return  a.lastName.localeCompare(b.lastName)
+// })
+// console.log(sortlash);
+
+
+
+// =======================================================================
+
+
+// Question:
+
+// 1. id si 5 ga teng bo'lgan odamni ma'lumotini manashu object ga almashtirib bering.
+// {id: 5, firstName: "Ali", lastName: "Valiyev", age: 25, status: "Dasturchi"}
+
+// 2. people array ichida lastName ning oxiri “va” qo’shimchasi bilan tugagan shaxslar qolsin.
+
+// 3. id si 4 va 9 ga teng bo’lgan odamlar arraydan o’chirib tashlansin.
+
+// 4. id si 7 ga teng bo’lgan object firstName ning qiymatini “Gulbashakar” ga o’zgartirib bering.
+
+// 5. shu people arrayni lastName bo’yicha sort qilib bering.
