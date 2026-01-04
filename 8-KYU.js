98a1dc006dca12933454e9dbb97a29f881dfcb6f
Asadbek <atamirzayevasadbek90@gmail.com>
2026-01-04 19:43:05 +0500

    change

--- 8-KYU.js
+++ 8-KYU.js
@@ -255,3 +255,21 @@
 //       return Number(area.toFixed(2));
 //     }).sort((a, b) => a - b);
 // }
+
+// 24 Who likes it
+// function likes(names) {
+//   const count = names.length;
+
+//   switch (count) {
+//     case 0:
+//       return "no one likes this";
+//     case 1:
+//       return `${names[0]} likes this`;
+//     case 2:
+//       return `${names[0]} and ${names[1]} like this`;
+//     case 3:
+//       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//     default:
+//       return `${names[0]}, ${names[1]} and ${count - 2} others like this`;
+//   }
+// }
