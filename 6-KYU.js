9739a2d9e1cf32c1e89e43069788829232f7b804
Asadbek <atamirzayevasadbek90@gmail.com>
2026-01-05 15:48:13 +0500

    change

---
+++ 6-KYU.js
@@ -0,0 +1,14 @@
+// 1 Create Phone Number 
+// function createPhoneNumber(numbers){
+//  return `(${numbers.slice(0, 3).join("")}) ${numbers.slice(3, 6).join("")}-${numbers.slice(6).join("")}`;
+// }
+
+// 2 Array.diff
+// function arrayDiff(a, b) {
+//   return a.filter((item) => !b.includes(item))
+// }
+
+// 3 Bit Counting
+// function countBits(n) {
+// return n.toString(2).split("").filter((bit) => bit === "1").length;
+// }
--- 8-KYU.js
+++ 8-KYU.js
@@ -274,18 +274,5 @@
 //   }
 // }
 
-// 25 Create Phone Number 
-// function createPhoneNumber(numbers){
-//  return `(${numbers.slice(0, 3).join("")}) ${numbers.slice(3, 6).join("")}-${numbers.slice(6).join("")}`;
-// }
 
-// 26 Array.diff
-// function arrayDiff(a, b) {
-//   return a.filter((item) => !b.includes(item))
-// }
-
-// 27 Bit Counting
-// function countBits(n) {
-// return n.toString(2).split("").filter((bit) => bit === "1").length;
-// }
 
